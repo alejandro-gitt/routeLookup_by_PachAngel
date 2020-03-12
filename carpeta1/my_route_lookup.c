d4960f7c588caf290ff97185fce4566461616138
angel-gitt <100384065@alumnos.uc3m.es>
2020-03-12 18:44:01 +0100

    Añadida funcion crearNodo y probada en main con exito

--- carpeta1/my_route_lookup.c
+++ carpeta1/my_route_lookup.c
@@ -6,24 +6,63 @@ enum flag{
   no = 0
 };
 
-typedef struct
-{
+typedef struct prefijo{
   enum flag marker_flag;
   enum flag prefix_flag;
   uint32_t prefijo;// primer bit: 1-> prefix;
   short siguiente_salto;
 }prefijo;
 
-typedef struct {
+typedef struct nodo{
   char n;
   prefijo *tabla;
-  nodo *left;
-  nodo *right;
+  struct nodo *left;
+  struct nodo *right;
 }nodo;
 
-int main(void)
-{
-prefijo *tabla = malloc(sizeof(prefijo)*2);
-free(tabla);
-return 0;
+nodo *crearNodo(nodo *raiz, char n){
+  char n_aux = raiz->n;
+  if(n_aux == n) return NULL;
+  else{
+    if(n < n_aux){
+      if(raiz->left != NULL) return crearNodo(raiz->left,n);
+      else{
+        nodo *nodo_aux = (nodo*)malloc(sizeof(nodo));
+        nodo_aux->n = n_aux/2;
+        nodo_aux->tabla = (prefijo*)malloc(sizeof(prefijo)*2);
+        nodo_aux->left = NULL;
+        nodo_aux->right = NULL;
+        raiz->left = nodo_aux;
+        return nodo_aux;
+      }
+    }
+    else{
+      if(raiz->right != NULL) return crearNodo(raiz->right,n);
+      else{
+        nodo *nodo_aux = (nodo*)malloc(sizeof(nodo));
+        nodo_aux->n = n_aux*2;
+        nodo_aux->tabla = (prefijo*)malloc(sizeof(prefijo)*2);
+        nodo_aux->left = NULL;
+        nodo_aux->right = NULL;
+        raiz->right = nodo_aux;
+        return nodo_aux;
+      }
+    }
+  }
+  return NULL;
+}
+
+int main(void){
+  nodo *raiz = (nodo*)malloc(sizeof(nodo));
+  raiz->n = 16;
+  raiz->tabla = (prefijo*)malloc(sizeof(prefijo)*2);
+  raiz->left = NULL;
+  raiz->right = NULL;
+  crearNodo(raiz,6);
+  free(raiz->left->tabla);
+  printf("1\n");
+  free(raiz->left);
+  free(raiz->tabla);
+  free(raiz);
+  return 0;
 }
