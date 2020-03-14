1bd59a745218abac2d4554281f1d4d5aaabcfe46
alejandro-gitt <100384019@alumnos.uc3m.es>
2020-03-14 14:09:58 +0100

    Añadida funcion de ampliacion de memoria

--- carpeta1/my_route_lookup.c
+++ carpeta1/my_route_lookup.c
@@ -1,5 +1,6 @@
 #include "io.h"
 #include "utils.h"
+#define COEFICIENTE 2
 
 enum flag{
   si = 1,
@@ -20,6 +21,29 @@ typedef struct {
   nodo *left;
   nodo *right;
 }nodo;
+
+/*
+método que recibe una tabla con una memoria insuficiente, se le adjudica más memoria,
+de momento una cantidad fija (mediante realloc)
+entradas:
+  - Puntero a la tabla
+  - Puntero a tabla nueva
+salidas:z
+  - nuevo puntero con mayor espacio reservado (COEFICIENTE*tamaño_anterior)
+*/
+
+int redimensiona(void *tabla_in){// Puede que debamos añadir una segunda entrada con el nuevo tamaño, de momento es un valor fijo(COEFICIENTE)
+    void *pnt_tmp = realloc(tabla_in , COEFICIENTE*sizeof(tabla_in));
+
+    if(pnt_tmp == NULL){
+      printf("%s\n","Asignación fallida");
+      return 0;
+    }else{
+      //Asignación conseguida
+      tabla_in = pnt_tmp;
+      return 1;
+    }
+}
 
 int main(void)
 {
