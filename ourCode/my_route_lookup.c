65d2cfba07c8c384a138f4ca9fadc9f669d91566
angel-gitt <100384065@alumnos.uc3m.es>
2020-04-03 19:38:27 +0200

    DEFINITIVO

--- ourCode/my_route_lookup.c
+++ ourCode/my_route_lookup.c
@@ -13,7 +13,7 @@ struct nodo{
   nodo *parentNode;
   nodo *left;
   nodo *right;
-  nodo *nextToMark;
+  nodo *nextToMark;//Relaciona los nodos a los que deberán añadirse markers (Ya que los guardamos en una LinkedList)
 };
 
 struct entrada{
@@ -21,9 +21,15 @@ struct entrada{
   char prefix_flag;
   uint32_t prefijo;
   short siguiente_salto;
-  entrada *next;
+  entrada *next;//En cada posición de la tabla puede haber varios prefijos, los enlazamos con una LinkedList
 };
 
+/*
+crearNodo:
+funcion que utilizamos para crear el arbol.
+Crea el nodo solicitado y los que se necesitan para llegar a él.
+Devuelve el nodo solicitado.
+*/
 nodo *crearNodo(nodo *raiz, char n, char param_nivel){
   char n_aux = raiz->n;
   if(n_aux == n) return NULL;
@@ -53,7 +59,7 @@ nodo *crearNodo(nodo *raiz, char n, char param_nivel){
         nodo_aux->right = NULL;
         nodo_aux->nextToMark = NULL;
         if(nodo_aux->n == 30){//Construcción del los nodos por debajo del 30 (29, 31 y 32)
-          //manualmente, ya que esta parte del arbol no sigue el mismo patrón.
+          //manualmente, ya que esta parte del árbol no sigue el mismo patrón.
           //Los nodos 29 y 32 serán descendientes del 30 y el 31 será descendiente del 32
           nodo_aux->right = (nodo*)calloc(1,sizeof(nodo));
           nodo_aux->right->n = 32;
@@ -88,7 +94,13 @@ nodo *crearNodo(nodo *raiz, char n, char param_nivel){
   return NULL;
 }
 
+/*
+calc_next_hop:
+Devuelve el siguiente salto ajustando al prefijo más largo buscado el
+sub-arbol que tiene como raiz el nodo que recibe en el primer parámetro.
+*/
 short calc_next_hop(nodo *raiz, uint32_t dir, short defaultInterface, int *numberOfTableAccesses){
+
   int netmask = 0;
   getNetmask(raiz->n,&netmask);
   uint32_t prefix = dir & (uint32_t)netmask;
@@ -96,10 +108,11 @@ short calc_next_hop(nodo *raiz, uint32_t dir, short defaultInterface, int *numbe
   nodo *currentNode = raiz;
   entrada *currentItem = NULL;
   *numberOfTableAccesses += 1;
-  while (currentNode != NULL){
+
+  while (currentNode != NULL){//Recorre el arbol hasta las hojas
     currentItem = &currentNode->tabla[hash(prefix >> (32-currentNode->n),currentNode->size_tabla)];
-    if(currentItem->prefix_flag != 0 || currentItem->marker_flag != 0){
-      while(currentItem->prefijo != prefix){
+    if(currentItem->prefix_flag != 0 || currentItem->marker_flag != 0){//Comprueba si hay algo en esa posición de la tabla
+      while(currentItem->prefijo != prefix){//Recorre la lista enlazada
         if(currentItem->next != NULL){
           currentItem = currentItem->next;
           *numberOfTableAccesses += 1;
@@ -109,16 +122,16 @@ short calc_next_hop(nodo *raiz, uint32_t dir, short defaultInterface, int *numbe
     if(currentItem->prefijo == prefix && (currentItem->marker_flag != 0 || currentItem->prefix_flag != 0)){
       if(currentItem->siguiente_salto != 0) next_hop = currentItem->siguiente_salto;
       if(currentItem->marker_flag != 0){
-        currentNode = currentNode->right;
+        currentNode = currentNode->right;//Ha habido match con un marker, seguimos buscando hacia la derecha (prefijos más largos)
       }
-      else break;
+      else break;//Ha habido match con un prefijo, ya tenemos el siguiente salto definitivo
     }else{
-      currentNode = currentNode->left;
+      currentNode = currentNode->left;//No ha habido match, seguimos buscando hacia la izquierda (prefijos más cortos)
     }
     if(currentNode != NULL){
       getNetmask(currentNode->n,&netmask);
     }else{
-      break;
+      break;//Hemos llegado al final del arbol, salimos con el salto del match más largo que hemos encontrado
     }
     prefix = dir & netmask;
     *numberOfTableAccesses += 1;
@@ -126,13 +139,15 @@ short calc_next_hop(nodo *raiz, uint32_t dir, short defaultInterface, int *numbe
   return next_hop;
 }
 
+/*
+addMarker:
+Añade markers a los nodos que estén en la lista enlazada encabezada por el nodo firstInList
+*/
 void addMarker(uint32_t dir,int prefixLength, short defaultInterface, nodo *firstInList,int *numberOfTableAccesses){
   entrada *currentItem = NULL;
   nodo *currentNode = firstInList;
   int netmask;
   uint32_t prefix;
-
-
 
   while(currentNode != NULL){//recorre la lista de nodos en los que hay que añadir markers
     getNetmask(currentNode->n,&netmask);
@@ -158,6 +173,10 @@ void addMarker(uint32_t dir,int prefixLength, short defaultInterface, nodo *firs
   }
 }
 
+/*
+free_hash_list:
+libera la memoria reservada para las listas enlazadas de la tabla hash
+*/
 void free_hash_list(entrada *head){
   if(head != NULL){
     if(head->next != NULL){
@@ -167,6 +186,10 @@ void free_hash_list(entrada *head){
   }
 }
 
+/*
+free_tree:
+libera la memoria reservada para el arbol y sus tablas
+*/
 void free_tree(nodo *raiz){
   if(raiz->left != NULL) free_tree(raiz->left);
   if(raiz->right != NULL) free_tree(raiz->right);
@@ -181,6 +204,10 @@ void free_tree(nodo *raiz){
   return;
 }
 
+/*Impresión post-order del árbol
+extraído de
+https://www.tutorialesprogramacionya.com/cya/detalleconcepto.php?punto=52&codigo=52&inicio=45
+*/
 void imprimirPost(nodo *raiz)
 {
     if (raiz != NULL)
@@ -207,14 +234,13 @@ int main(int argc, char *argv[]){
   short defaultInterface = 0;
   int numberOfTableAccesses = 0;
 
-  nodo *currentNode = NULL;
-  nodo* headNode = NULL;
-  nodo* tailNode = NULL;
-  nodo *currentLista = NULL;
-  entrada *currentItem = NULL;
+  nodo *currentNode = NULL;//Para recorrer el arbol
+  nodo* headNode = NULL;//Primer elemento de la lista de nodos en los que hay que introducir markers
+  nodo* tailNode = NULL;//Último elemento de la lista de nodos en los que hay que introducir markers
+  entrada *currentItem = NULL;//Para recorrer la lista enlazada de la tabla hash
   int counter = 0;
 
-  nodo *raiz = (nodo*)calloc(1,sizeof(nodo));
+  nodo *raiz = (nodo*)calloc(1,sizeof(nodo));//Creamos el nodo raíz, 16 para un arbol balanceado
   raiz->n = 16;
   raiz->tabla = (entrada*)calloc(TAMANO_INICIAL,sizeof(entrada));
   raiz->size_tabla = TAMANO_INICIAL;
@@ -230,7 +256,7 @@ int main(int argc, char *argv[]){
     return -1;
   }
 
-  do{
+  do{//Bucle para ir leyendo e introduciendo cada prefijo en el arbol
     errno = readFIBLine(&dir, &prefixLength, &outInterface);
     if(prefixLength == 0){
       defaultInterface = outInterface;
@@ -245,6 +271,8 @@ int main(int argc, char *argv[]){
     }
     else if(errno == OK){
 
+      /* Buscamos el nodo correspondiente. Si no existe se crea,
+      formando así el arbol progresivamente*/
       while(currentNode->n != prefixLength){
         if(currentNode->n > prefixLength){
           if(currentNode->left == NULL) crearNodo(raiz,prefixLength,raiz->n/2);
@@ -252,7 +280,7 @@ int main(int argc, char *argv[]){
         }
         else{
           if(currentNode->right == NULL) crearNodo(raiz,prefixLength,raiz->n/2);
-          /*Añadiendo currentNode a la lista para añadir marker*/
+          /* Añadiendo currentNode a la lista para añadir marker */
           if(headNode == NULL){
             headNode = currentNode;
             tailNode = headNode;
@@ -264,10 +292,8 @@ int main(int argc, char *argv[]){
           currentNode = currentNode->right;
         }
       }
-      currentLista = headNode;
-      while(currentLista != NULL){
-        currentLista = currentLista->nextToMark;
-      }
+
+      /* Introducimos el prefijo en su nodo y los markers donde corresponde: */
       currentItem = &currentNode->tabla[hash(prefix >> (32-prefixLength),currentNode->size_tabla)];
       if(currentItem->prefix_flag != 0 || currentItem->marker_flag != 0){
         while(currentItem->prefijo != prefix){
@@ -282,15 +308,15 @@ int main(int argc, char *argv[]){
       currentItem->prefijo = prefix;
       currentItem->prefix_flag = 1;
       currentItem->siguiente_salto = (short)outInterface;
-      if(headNode != NULL){//hay que añadir markers
+      if(headNode != NULL){//Hay que añadir markers
         addMarker(dir, prefixLength, defaultInterface, headNode, &numberOfTableAccesses);
       }
       counter += 1;
       headNode = NULL;
-      currentLista = NULL;
       tailNode = NULL;
     }
   }while(errno != REACHED_EOF);
+
   errno = 0;
   dir = 0;
   short siguiente_salto = 0;
@@ -301,6 +327,10 @@ int main(int argc, char *argv[]){
   numberOfTableAccesses = 0;
   int totalTableAccesses = 0;
   counter = 0;
+
+  /*
+  A continuación, leemos y procesamos las direcciones, calculando su siguiente salto.
+  */
   while(1){
     errno = readInputPacketFileLine(&dir);
     if(errno == REACHED_EOF) break;
@@ -314,7 +344,6 @@ int main(int argc, char *argv[]){
     numberOfTableAccesses = 0;
     counter += 1;
   }
-
   printSummary(counter, totalTableAccesses/counter, TotalTime/counter);
   free_tree(raiz);
   freeIO();
